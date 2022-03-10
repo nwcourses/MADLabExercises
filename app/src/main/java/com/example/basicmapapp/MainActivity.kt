eb7175a9deb4c611eb825314f068725fe89f0057
Nick Whitelegg <nickw4426@gmail.com>
2022-03-10 15:04:07 +0000

    Week 3 exercise 3

--- app/src/main/java/com/example/basicmapapp/MainActivity.kt
+++ app/src/main/java/com/example/basicmapapp/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -26,5 +28,14 @@ class MainActivity : AppCompatActivity() {
         val map1 = findViewById<MapView>(R.id.map1)
         map1.controller.setZoom(16.0)
         map1.controller.setCenter(GeoPoint(51.05, -0.72))
+
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val etLat = findViewById<EditText>(R.id.etLat)
+        val etLon = findViewById<EditText>(R.id.etLon)
+        btn1.setOnClickListener {
+            val lat = etLat.text.toString().toDouble()
+            val lon = etLon.text.toString().toDouble()
+            map1.controller.setCenter(GeoPoint(lat, lon))
+        }
     }
 }
